59bfe6c3c28e0508a7b032ba53d116b0a34af713
unknown <vidhiagrawal305@gmail.com>
2020-05-20 13:05:38 +0530

    msg

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A test Recipe', 'This is simply a test', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2F1496939575%2Fhot-dog-nachos-XL-RECIPE2017.jpg')
+];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
